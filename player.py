37411c502bae0863ebade164b7f6557b57ebcf29
Elias Valdez <evaldez@linode.com>
2024-10-16 17:25:40 -0400

    Player now moves vertically

--- player.py
+++ player.py
@@ -10,15 +10,17 @@ class Player(circleshape.CircleShape):
         self.rotation = 0
 
     def triangle(self):
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        forward = pygame.Vector2(0, -1).rotate(self.rotation)
+        right = pygame.Vector2(
+            0, -1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
     def draw(self, screen):
-        pygame.draw.polygon(screen, constants.RGB_WHITE, self.triangle(), width=2)
+        pygame.draw.polygon(screen, constants.RGB_WHITE,
+                            self.triangle(), width=2)
 
     def rotate(self, dt):
         self.rotation += constants.PLAYER_TURN_SPEED * dt
@@ -30,3 +32,12 @@ class Player(circleshape.CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        """Vertical movement logic"""
+        forward = pygame.Vector2(0, -1).rotate(self.rotation)
+        self.position += forward * constants.PLAYER_SPEED * dt
